016ffd69c354322d478c8325c99065c424f1c311
eniyavignesh <130365551+eniyavignesh@users.noreply.github.com>
2023-04-11 17:33:41 +0530

    Add files via upload

---
+++ Com.dal.threadseg/src/com/dal/threadseg/MyExecutor.java
@@ -0,0 +1,22 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dal.threadseg;
+
+import java.util.concurrent.Executor;
+
+/*
+ The java.util.concurrent package defines a set of interfaces whose implementations execute tasks. The simplest one of
+these is the Executor interface:
+
+public interface Executor {
+void execute(Runnable command);
+}
+Hence an Executor implementation takes the given Runnable instance and executes it. The interface makes no assumptions
+about the way of the execution, the javadoc only states "Executes the given command at some time in the future.".
+ 
+ */
+
